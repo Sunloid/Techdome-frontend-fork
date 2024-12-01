d9320def2a416367c1f907fe9905aa1c43ae99bd
Sunloid <ahmedrizavi04@gmail.com>
2024-12-01 15:49:28 +0530

    Frontend

--- src/apiConfig/api.js
+++ src/apiConfig/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://13.232.245.205:8080/api";
+const baseUrl = "http://localhost:5000/api";
 
 const instance = axios.create({
     baseURL: `${baseUrl}`
